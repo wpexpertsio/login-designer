38021166964139636bdd58fb1286b1805c25862b
Richard Tabor <hello@themebeans.com>
2017-10-20 14:08:27 -0400

    Minor style improvements within the Customizer

--- assets/js/login-designer-customize-events.js
+++ assets/js/login-designer-customize-events.js
@@ -94,10 +94,6 @@
 
 		controls.forEach( function( item, index, array ) {
 
-			if ( item[0] ) {
-				wp.customize.control( item ).container.addClass( 'is-active' );
-			}
-
 			if ( action == 'activate' ) {
 
 				// For this particular control, let's check to see if corresponding options are visible.
