3502486769b58b471b56c8d68f01a44fd68fb02e
Richard Tabor <hello@themebeans.com>
2017-10-25 18:08:59 -0400

    Removed message

--- includes/customize-settings/settings.php
+++ includes/customize-settings/settings.php
@@ -40,15 +40,15 @@ $wp_customize->add_control( 'login_designer_admin[logout_redirect]', array(
 	'allow_addition' 	=> true,
 ) );
 
-$wp_customize->add_setting( 'login_designer_admin[login_message]', array(
-	'default'               => $admin_defaults['login_message'],
-	'type' 			=> 'option',
-	// 'transport'         	=> 'postMessage',
-	'sanitize_callback' 	=> 'esc_textarea',
-) );
+// $wp_customize->add_setting( 'login_designer_admin[login_message]', array(
+// 	'default'               => $admin_defaults['login_message'],
+// 	'type' 			=> 'option',
+// 	// 'transport'         	=> 'postMessage',
+// 	'sanitize_callback' 	=> 'esc_textarea',
+// ) );
 
-$wp_customize->add_control( 'login_designer_admin[login_message]', array(
-	'label'          	=> esc_html__( 'Login Message', '@@textdomain' ),
-	'section'        	=> 'login_designer__section--settings',
-	'type'           	=> 'textarea',
-) );
+// $wp_customize->add_control( 'login_designer_admin[login_message]', array(
+// 	'label'          	=> esc_html__( 'Login Message', '@@textdomain' ),
+// 	'section'        	=> 'login_designer__section--settings',
+// 	'type'           	=> 'textarea',
+// ) );
