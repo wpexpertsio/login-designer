724c32a79f0e8ec72e043abfe3a5558da525370e
Richard Tabor <hello@themebeans.com>
2017-11-02 13:46:34 -0400

    Return early if there's no pro version yet.

--- includes/settings/license.php
+++ includes/settings/license.php
@@ -8,10 +8,14 @@
  * @license   @@pkg.license
  */
 
+// Return early, if there is no pro version yet.
+if ( ! Login_Designer()->has_pro() ) {
+	return;
+}
+
 /**
  * License Section.
  */
-
 $url = esc_url( add_query_arg( array(
 	'utm_source'   => 'customizer',
 	'utm_medium'   => 'license-key-description',
@@ -20,20 +24,17 @@ $url = esc_url( add_query_arg( array(
 	),
 'https://logindesigner.com/pricing' ) );
 
-if ( Login_Designer()->has_pro() ) {
+$wp_customize->add_setting( 'login_designer_license[key]', array(
+	'default'             	=> '',
+	'transport'             => 'postMessage',
+	'type' 			=> 'option',
+	'sanitize_callback'     => 'sanitize_text_field',
+) );
 
-	$wp_customize->add_setting( 'login_designer_license[key]', array(
-		'default'             	=> '',
-		'transport'             => 'postMessage',
-		'type' 			=> 'option',
-		'sanitize_callback'     => 'sanitize_text_field',
-	) );
-
-	$wp_customize->add_control( new Login_Designer_License_Control( $wp_customize, 'login_designer_license[key]', array(
-		'type'                  => 'login-designer-title',
-		'label'                 => esc_html__( 'License', '@@textdomain' ),
-		'description'           => sprintf( esc_html__( 'Enter a %slicense key%s to enable remote updates, unlock premium templates and activate professional extensions.', '@@textdomain' ), '<a href="' . esc_url( $url ) . '" target="_blank">', '</a>' ),
-		'section'               => 'login_designer__section--settings',
-		'priority' 		=> 1,
-	) ) );
-}
+$wp_customize->add_control( new Login_Designer_License_Control( $wp_customize, 'login_designer_license[key]', array(
+	'type'                  => 'login-designer-title',
+	'label'                 => esc_html__( 'License', '@@textdomain' ),
+	'description'           => sprintf( esc_html__( 'Enter a %slicense key%s to enable remote updates, unlock premium templates and activate professional extensions.', '@@textdomain' ), '<a href="' . esc_url( $url ) . '" target="_blank">', '</a>' ),
+	'section'               => 'login_designer__section--settings',
+	'priority' 		=> 1,
+) ) );
