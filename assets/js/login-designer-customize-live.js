229350d8cff9572091c8eb6a3eeb17924f7fd192
Richard Tabor <hello@themebeans.com>
2017-12-15 10:42:07 -0500

    Modified the background shortcut button to be a paintbrush, instead of a pencil (as the form bg is)

--- assets/js/login-designer-customize-live.js
+++ assets/js/login-designer-customize-live.js
@@ -52,7 +52,7 @@
 				$username_label.append( '<button class="login-designer-event-button customizer-event-overlay" data-customizer-event="login-designer-edit-loginform-labels-username"></button>' );
 				$password_label.append( '<button class="login-designer-event-button customizer-event-overlay" data-customizer-event="login-designer-edit-loginform-labels-password"></button>' );
 
-				$body_bg.append( '<span class="customize-partial--login-designer-background customize-partial-edit-shortcut"><button class="login-designer-event-button customize-partial-edit-shortcut-button" data-customizer-event="login-designer-edit-background"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13.89 3.39l2.71 2.72c.46.46.42 1.24.03 1.64l-8.01 8.02-5.56 1.16 1.16-5.58s7.6-7.63 7.99-8.03c.39-.39 1.22-.39 1.68.07zm-2.73 2.79l-5.59 5.61 1.11 1.11 5.54-5.65zm-2.97 8.23l5.58-5.6-1.07-1.08-5.59 5.6z"></path></svg></button></span>' );
+				$body_bg.append( '<span class="customize-partial--login-designer-background customize-partial-edit-shortcut"><button class="login-designer-event-button customize-partial-edit-shortcut-button" data-customizer-event="login-designer-edit-background"></button></span>' );
 
 				$button.append( '<button class="login-designer-event-button customizer-event-overlay" data-customizer-event="login-designer-edit-button"></button>' );
 
