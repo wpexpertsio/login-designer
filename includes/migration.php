f44e143307ea4cd9d0de0e805f3c86aa8783bf62
Richard Tabor <hello@themebeans.com>
2018-02-06 10:44:26 -0500

    Minor touchups/commenting

--- includes/migration.php
+++ includes/migration.php
@@ -8,7 +8,9 @@
  */
 
 /**
- * Migrate the button's height to the new padding top and padding bottom options.
+ * Migrate the button height to the new padding top and padding bottom options.
+ *
+ * @since 1.1.5
  */
 function login_designer_button_padding() {
 
@@ -18,25 +20,28 @@ function login_designer_button_padding() {
 	// Current site options.
 	$current_options = get_option( 'login_designer', array() );
 
-	// Check if options exist.
+	// Check if options exist. If not, return.
 	if ( ! $current_options ) {
 		return false;
 	}
 
-	// Check if the button_height option exists.
-	if ( isset( $current_options['button_height'] ) ) {
-		$options['button_padding_top']    = $current_options['button_height'] / 2;
-		$options['button_padding_bottom'] = $current_options['button_height'] / 2;
+	// Check if the button_height option exists. If not, return.
+	if ( ! isset( $current_options['button_height'] ) ) {
+		return false;
 	}
+
+	// Let's set the padding options to half of the button_height.
+	$options['button_padding_top']    = $current_options['button_height'] / 2;
+	$options['button_padding_bottom'] = $current_options['button_height'] / 2;
 
 	// Merge options.
 	$merged_options = array_merge( $current_options, $options );
 	$new_options    = $merged_options;
 
-	// Update new licensing.
+	// Update the options.
 	update_option( 'login_designer', $new_options );
 
-	// Remove old settings.
+	// Remove the old option.
 	delete_option( $current_options['button_height'] );
 }
 add_action( 'after_setup_theme', 'login_designer_button_padding' );
