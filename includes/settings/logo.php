533227347bab7555386119e6ba4aadee5d041926
Richard Tabor <hello@themebeans.com>
2018-02-07 11:45:50 -0500

    PHPCS

--- includes/settings/logo.php
+++ includes/settings/logo.php
@@ -11,7 +11,7 @@
  * Settings.
  */
 $wp_customize->add_setting( 'login_designer[logo_title]', array(
-	'sanitize_callback'     => 'sanitize_text_field',
+	'sanitize_callback' => 'sanitize_text_field',
 ) );
 
 $wp_customize->add_control( new Login_Designer_Title_Control( $wp_customize, 'login_designer[logo_title]', array(
