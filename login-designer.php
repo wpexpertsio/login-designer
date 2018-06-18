ad728f913d3ab47600a77f8cd9bb3462fd883c3a
Rich Tabor <hi@richtabor.com>
2018-06-18 16:06:20 -0400

    v1.1.9

--- login-designer.php
+++ login-designer.php
@@ -5,7 +5,7 @@
  * Description: @@pkg.description
  * Author: @@pkg.author
  * Author URI: @@pkg.author_uri
- * Version: 1.1.8
+ * Version: 1.1.9
  * Text Domain: @@textdomain
  * Domain Path: languages
  * Requires at least: @@pkg.requires
