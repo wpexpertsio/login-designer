b45b4d8729cabcc6cc03727cdcdb831d242efbfd
Rich Tabor <hi@richtabor.com>
2019-11-12 18:31:44 -0500

    Update login-designer.php

--- login-designer.php
+++ login-designer.php
@@ -7,7 +7,7 @@
  * Author URI:      https://logindesigner.com
  * Text Domain:     login-designer
  * Domain Path:     /languages
- * Version:         1.1.12
+ * Version:         1.2.0
  *
  * Login Designer is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
